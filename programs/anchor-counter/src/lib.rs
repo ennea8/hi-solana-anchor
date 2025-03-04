41d403a2cd13f7674261479e300cd2b066562f07
ennea8 <ennea8@gmail.com>
2025-03-04 16:41:52 +0800

    add anchor counter

---
+++ programs/anchor-counter/src/lib.rs
@@ -0,0 +1,55 @@
+use anchor_lang::prelude::*;
+
+declare_id!("9uyaujBebdYM9gNdnreC4s8ivjT68Hy7HhDdAyWidVUt");
+
+#[program]
+pub mod anchor_counter {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("Counter Account Created");
+        msg!("Current Count: { }", counter.count);
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Update>, increment_by: u64) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        // counter.count += increment_by;
+        msg!("Previous counter: {}", counter.count);
+        counter.count = counter.count.checked_add(increment_by).unwrap();
+        msg!("Current Count: { }", counter.count);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = DISCRIMINATOR + Counter::INIT_SPACE
+    )]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>, // payer for the transaction fee
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+
+// TODO 这是一个有密钥的地址？ 并且可以存储数据？
+// 运行示类例代码？
+#[account]
+#[derive(InitSpace)]
+pub struct Counter {
+    pub count: u64,
+}
+
+const DISCRIMINATOR: usize = 8;
--- programs/hi-solana-anchor/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use anchor_lang::prelude::*;
-
-declare_id!("HUUAj59uBGfY74wXDbEtf3kvp1X4kPhVdVwEyc5tMdMg");
-
-#[program]
-pub mod anchor_counter {
-    use super::*;
-}
